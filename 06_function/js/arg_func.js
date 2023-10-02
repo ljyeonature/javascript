78451d21d249ec91e33aa416e9811f045578882e
unknown <ljyeon131@gmail.com>
2023-10-02 14:22:54 +0900

    1002JS

--- 06_function/js/arg_func.js
+++ 06_function/js/arg_func.js
@@ -2,18 +2,20 @@
 function arrayProcess( data, f ) {
 	for( key=0; key < data.length; key++ ){
 	//for(var key in data ){
-		f(  data[key].value );
+		f( data[key].value );
 	}
 }
 
 // 각각의 데이터를 더하는 함수
 var sumResult = 0;
+// sum 함수
 function sum(  data ){	
 	sumResult += parseInt(data);
 }
 
 // 각각의 데이터의 곱을 구하는 함수
 var squareResult = new Array();
+// var squareResult = [];
 function square(  data ){			
 	squareResult.push( data * data );
 }
